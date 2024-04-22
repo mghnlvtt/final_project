752eb8e6e568948ae9bff5965f3e8c538772591a
mghnlvtt <122109254+mghnlvtt@users.noreply.github.com>
2024-04-22 13:58:08 -0400

    Create test.js

---
+++ js/test.js
@@ -0,0 +1,19 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const APIController = (function() {
+        const id = '/*INSERT_CLIENT_ID_HERE*/';
+        const secret = '/*INSERT_CLIENT_SECRET_HERE*/';
+        const testing = () => {
+            const token = btoa(id + ':' + secret);
+            console.log('id:', id);
+            console.log('secret:', secret);
+            console.log('test:', token);
+        };
+
+        return {
+            testing
+        };
+    })();
+
+    APIController.testing();
+});
