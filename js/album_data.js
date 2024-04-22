94e3e2d200e9e11568cc7cfe85b6aaf7eb7ef363
mghnlvtt <122109254+mghnlvtt@users.noreply.github.com>
2024-04-22 22:33:08 +0000

    Deploying to main from @ mghnlvtt/final_project@061191a16957373b970cb4b76b1f4dd4971ef9d1 🚀

--- js/album_data.js
+++ js/album_data.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const API = (() => {
-        const id = '/*INSERT_CLIENT_ID_HERE*/';
-        const secret = '/*INSERT_CLIENT_SECRET_HERE*/';
+        const id = '4dd98c178ff74d3a82dc42f537ad5736';
+        const secret = 'fac25ea5a34d4e5fab4be12cc29f6238';
 
         const getToken = async () => {
             const result = await fetch('https://accounts.spotify.com/api/token', {
